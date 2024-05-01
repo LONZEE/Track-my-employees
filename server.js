ed6fa36ebf91807216135f78491a80bc09b870e6
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-01 13:17:48 -0500

    working branch

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: "root",
     password: "123456789",
-    database: "employee_db",
+    database: "employeeTracker_db",
     port: 3306,
 });
 
