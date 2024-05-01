a004b606b147f1fe11f346daabb15652d9e24e17
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-01 13:15:25 -0500

    working branch

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO departments (department_name)
+VALUES 
+('Executive Board'),
+('Marketing'),
+('Human Resources'),
+('Finance'),
+('Engineering'),
+('Information Technology'),
+('Customer Relations'),
+('Research and Development'),
+('Legal'),
+('Maintenance');
